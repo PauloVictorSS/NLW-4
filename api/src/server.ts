97bc26c42724aff1e26abd533e239f1f67243f85
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2021-02-23 10:18:01 -0300

    Dia 2 - 23/02
    - Criação de rotas;
    - Criação de migrations;
    - Adição de dois usuários no DB;

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,28 +1,11 @@
-import express from 'express'
+import 'reflect-metadata';
+import express from 'express';
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/* Possíveis funções:
- *
- * GET => Buscar
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-
-// http://localhost:3333/
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello word - NLW04" });
-});
-
-// 1 parâ => Rota(Recurso API)
-// 2 parâ => request, response
-
-app.post("/", (request, response)  =>{
-    //Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
